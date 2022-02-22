6f21db59587a249b70ad140f0e90abdc575ae11c
Brian Daley <brian.daley@uconn.edu>
2022-02-22 16:43:42 -0500

    search is working

--- js/internships.js
+++ js/internships.js
@@ -2,10 +2,36 @@ var app = new Vue({
   el: '#app',
   data: {
     internships: [],
+    companies: [],
+    searchText: ''
 
   },
+  methods: {
+    searchByKeyword: async function() {
+
+      this.reset();
+      let internships = await this.getData();
+
+      internships.forEach(row => {
+        if(row.title.toLowerCase().indexOf(this.searchText.toLowerCase()) !== -1 ||
+          row.description.toLowerCase().indexOf(this.searchText.toLowerCase()) !== -1){
+          this.internships.push(row)
+        }
+      });
+
+    },
+    getData: async function(){
+      const response = await fetch("internships.json");
+      return await response.json();
+    },
+    reset: function() {
+      this.internships = [];
+    }
+  },
   async created() {
-    const response = await fetch("https://bdaley.npkn.net/dmd-hub-json/internships");
-    this.internships = await response.json();
+    // const response = await fetch("https://bdaley.npkn.net/dmd-hub-json/internships");
+    this.internships = await this.getData();
+
+
   }
 })

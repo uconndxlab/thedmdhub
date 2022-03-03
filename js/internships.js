880b6e55f4d365e6c64a2de67d81d8e41e23b1dc
Natalie <natalie.lacroix@uconn.edu>
2022-03-03 01:01:48 -0500

    company filters

--- js/internships.js
+++ js/internships.js
@@ -12,10 +12,14 @@ let dateRanges = [
   'within last month',
 ];
 
+var companyvalue = ""
+var company = document.getElementById('companyFilter')
+var numresults = document.getElementById('numresults')
+
 
 async function getAllInternships(){
-  // const response = await fetch("https://bdaley.npkn.net/dmd-hub-json/internships");
-  const response = await fetch("internships.json");
+  const response = await fetch("https://bdaley.npkn.net/dmd-hub-json/internships?"+companyvalue);
+  //const response = await fetch("internships.json");
   return await response.json();
 }
 
@@ -35,11 +39,13 @@ function outputInternships() {
     `
   }
   document.querySelector('#course-list').innerHTML = output;
+  numresults.innerHTML="Results: "+internships.length
 }
 
 
 // Stuff to run when the DOM is ready
 window.addEventListener('DOMContentLoaded', async () =>{
+  
   internships = await getAllInternships()
 
   // Get lists for <select> boxes
@@ -48,14 +54,13 @@ window.addEventListener('DOMContentLoaded', async () =>{
     // Populate list of companies
     if(!companies.includes(row.company)){
       companies.push(row.company)
-      document.querySelector('#companyFilter').innerHTML += `<option>${row.company}</option>`
+      document.querySelector('#companyFilter').innerHTML += `<option value="${row.company.toLowerCase()}">${row.company}</option>`
     }
 
     // Populate list of concentrations
     if(!concentrations.includes(row.concentration)){
       concentrations.push(row.concentration)
       document.querySelector('#concentrationFilter').innerHTML += `<option>${row.concentration}</option>`
-
     }
 
     // Populate list of job types
@@ -71,6 +76,7 @@ window.addEventListener('DOMContentLoaded', async () =>{
     }
 
   outputInternships();
+  displayDropdown()
 
 })
 
@@ -89,6 +95,109 @@ document.querySelector('#searchBtn').addEventListener('click', async () =>{
   });
   outputInternships();
 })
+
+// company filtering
+
+async function companyFiltering(){
+  internships = [];
+  console.log('uh huh')
+  internships = await getAllInternships()
+  outputInternships();
+}
+
+// dropdown design
+function displayDropdown(){
+  var x, i, j, l, ll, selElmnt, a, b, c;
+/* Look for any elements with the class "custom-select": */
+x = document.getElementsByClassName("custom-select");
+l = x.length;
+for (i = 0; i < l; i++) {
+  selElmnt = x[i].getElementsByTagName("select")[0];
+  ll = selElmnt.length;
+  /* For each element, create a new DIV that will act as the selected item: */
+  a = document.createElement("DIV");
+  a.setAttribute("class", "select-selected");
+  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+  x[i].appendChild(a);
+  /* For each element, create a new DIV that will contain the option list: */
+  b = document.createElement("DIV");
+  b.setAttribute("class", "select-items select-hide");
+  for (j = 0; j < ll; j++) {
+    /* For each option in the original select element,
+    create a new DIV that will act as an option item: */
+    c = document.createElement("DIV");
+    c.innerHTML = selElmnt.options[j].innerHTML;
+    c.addEventListener("click", function(e) {
+        /* When an item is clicked, update the original select box,
+        and the selected item: */
+        var y, i, k, s, h, sl, yl;
+        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        sl = s.length;
+        h = this.parentNode.previousSibling;
+        
+        for (i = 0; i < sl; i++) {
+          if (s.options[i].innerHTML == this.innerHTML) {
+            s.selectedIndex = i;
+            h.innerHTML = this.innerHTML;
+            y = this.parentNode.getElementsByClassName("same-as-selected");
+            yl = y.length;
+            for (k = 0; k < yl; k++) {
+              y[k].removeAttribute("class");
+            }
+            this.setAttribute("class", "same-as-selected");
+            break;
+          }
+        }
+        h.click();
+
+        /*change value for fetch url based off dropdown*/
+        companyvalue = '&company='+company.value
+        if (company.value==='all'){
+          companyvalue = ""
+        }
+        companyFiltering()
+        
+
+    });
+    b.appendChild(c);
+  }
+  x[i].appendChild(b);
+  a.addEventListener("click", function(e) {
+    /* When the select box is clicked, close any other select boxes,
+    and open/close the current select box: */
+    e.stopPropagation();
+    closeAllSelect(this);
+    this.nextSibling.classList.toggle("select-hide");
+    this.classList.toggle("select-arrow-active");
+  });
+}
+
+function closeAllSelect(elmnt) {
+  /* A function that will close all select boxes in the document,
+  except the current select box: */
+  var x, y, i, xl, yl, arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  xl = x.length;
+  yl = y.length;
+  for (i = 0; i < yl; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i)
+    } else {
+      y[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (i = 0; i < xl; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
+
+/* If the user clicks anywhere outside the select box,
+then close all select boxes: */
+document.addEventListener("click", closeAllSelect);
+}
 
 
 
